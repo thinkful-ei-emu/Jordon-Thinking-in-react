dc55a383e45c492e12425dc6b11e1d9da458a996
Jvymvn <Jordon.carter@yahoo.com>
2019-07-13 21:17:12 -0400

    Updates

--- src/FeaturesList.js
+++ src/FeaturesList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Feature from './Feature'
 
-function FeaturesList(props) {
-    render(){
-        const features = Object.keys(props.features)
+class FeaturesList extends React.Component {
+    render() {
+        const features = Object.keys(this.props.features)
             .map(category => {
-                const options = props.features[category].map((item, index) => {
-                    const selectedClass = item.name === props.selected[category].name ? 'feature__selected' : '';
+                const options = this.props.features[category].map((item, index) => {
+                    const selectedClass = item.name === this.props.selected[category].name ? 'feature__selected' : '';
                     const featureClass = 'feature__option ' + selectedClass;
-                    return <Feature key={index} featureHeader={category} item={item} featureClass={featureClass} updateFeature={props.updateFeature} />
+                    return <Feature key={index} featureHeader={category} item={item} featureClass={featureClass} updateFeature={this.props.updateFeature} />
                 });
 
                 return <div className="feature" key={category}>
@@ -18,15 +18,16 @@ function FeaturesList(props) {
                     </ul>
                 </div>
             });
+
+
+        return (
+            <section className="main__form">
+                <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+                {features}
+            </section>
+
+        )
     }
-
-    return (
-        <section className="main__form">
-            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-            {features}
-        </section>
-
-    )
 }
 
 export default FeaturesList;
